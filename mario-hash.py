258b47f21ed4f47f06e40dc62ef246eb9827218e
Baha Rtemi <bahaalrtemi@yahoo.com>
2022-08-04 09:38:28 +0200

    Mario-Hash

---
+++ mario-hash.py
@@ -0,0 +1,26 @@
+print("One Line of Mario Hash")
+print()
+
+for i in range(3):
+    print("#", end="")
+    # end="\n" default value 
+    # You can use just print() just for new line
+
+print()
+print()
+print()
+
+# SAME
+print("#" * 3)
+
+print()
+
+print("#############################")
+print("#############################")
+
+print()
+
+for i in range(3):# Height
+    for j in range(4):# Width
+        print("#", end="")
+    print()# For New Line
